f4aff16b7363425ebfcf36056cf93e6a363a8c4b
anush-2007 <anushbhardwaj2007@gmail.com>
2024-12-05 21:04:09 +0530

    login commit

--- src/components/SignupSignin/index.js
+++ src/components/SignupSignin/index.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import './styles.css';
 import Input from '../Input';
 import Button from '../Button';
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 import { auth } from '../../firebase';
 import { toast } from 'react-toastify';
 
@@ -12,6 +12,7 @@ function SignupSigninComponent() {
   const [email, setEmail] =useState("");
   const [password, setPassword] =useState("");
   const [confirmPassword, setConfirmPassword] =useState("");
+  const [loginForm, setLoginForm] = useState(false);
   const [loading, setLoading] =useState(false);
 
   function signupWithEmail() {
@@ -51,50 +52,135 @@ function SignupSigninComponent() {
         toast.error("All fields are mandatory!")
         setLoading(false);
     }
-    
   }
+
+function loginUsingEmail() {
+  console.log("Email", email);
+  console.log("Password", password);
+  if( email != "" && password != "" ) {
+    signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+  } else {
+    toast.error("All fields are mandatory!")
+  }
+
+
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      // Signed in 
+      const user = userCredential.user;
+      // ...
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+    });
+}
+
+function createDoc() {
+  //Make sure that the doc with the uid does not exist
+  // Create a Doc
+}
+
   return (
-    <div className="signup-wrapper">
-      <h2 className="title">
-      Sign Up on <span style={{ color: "var(--theme)" }}>Financely</span>
-      </h2>
-      <form>
-        <Input 
-          type="text"
-          label={"Full Name"} 
-          state={name} 
-          setState={setName} 
-          placeholder={"John Snow"} 
-        />
-        <Input 
-          type="email"
-          label={"Email"} 
-          state={email} 
-          setState={setEmail} 
-          placeholder={"JohnSnow@gmail.com"} 
-        />
-        <Input
-          type="password" 
-          label={"Password"} 
-          state={password} 
-          setState={setPassword} 
-          placeholder={"Example@123"}
-        />
-        <Input
-          type="password" 
-          label={"Confrim Password"} 
-          state={confirmPassword} 
-          setState={setConfirmPassword} 
-          placeholder={"Example@123"} 
-        />
-        <Button 
-        disabled={loading}
-        text={loading ? "Loading..." : "Signup Using Email and Password"}
-        onClick={signupWithEmail} />
-        <p style={{ textAlign: "center" , margin:0 }}>or</p>
-        <Button text={loading ? "Loading..." : "Signup Using Google"} blue={true} />
-      </form>
-    </div>
+    <>
+    {loginForm ? 
+      (
+        <div className="signup-wrapper">
+          <h2 className="title">
+          Log In on <span style={{ color: "var(--theme)" }}>Financely</span>
+          </h2>
+          <form>
+            <Input 
+              type="email"
+              label={"Email"} 
+              state={email} 
+              setState={setEmail} 
+              placeholder={"JohnSnow@gmail.com"} 
+            />
+            <Input
+              type="password" 
+              label={"Password"} 
+              state={password} 
+              setState={setPassword} 
+              placeholder={"Example@123"}
+            />
+            <Button 
+              disabled={loading}
+              text={loading ? "Loading..." : "Login Using Email and Password"}
+              onClick={loginUsingEmail} 
+            />
+            <p className="p-login" >or</p>
+            <Button text={loading ? "Loading..." : "Login Using Google"} blue={true} />
+            <p 
+              className="p-login" 
+              style={{ cursor: "pointer" }} 
+              onClick={() => setLoginForm(!loginForm)}
+            >
+              Or Don't Have An Account? Click Here
+            </p>
+          </form>
+        </div>
+       ) : (
+        <div className="signup-wrapper">
+          <h2 className="title">
+          Sign Up on <span style={{ color: "var(--theme)" }}>Financely</span>
+          </h2>
+          <form>
+            <Input 
+              type="text"
+              label={"Full Name"} 
+              state={name} 
+              setState={setName} 
+              placeholder={"John Snow"} 
+            />
+            <Input 
+              type="email"
+              label={"Email"} 
+              state={email} 
+              setState={setEmail} 
+              placeholder={"JohnSnow@gmail.com"} 
+            />
+            <Input
+              type="password" 
+              label={"Password"} 
+              state={password} 
+              setState={setPassword} 
+              placeholder={"Example@123"}
+            />
+            <Input
+              type="password" 
+              label={"Confrim Password"} 
+              state={confirmPassword} 
+              setState={setConfirmPassword} 
+              placeholder={"Example@123"} 
+            />
+            <Button 
+              disabled={loading}
+              text={loading ? "Loading..." : "Signup Using Email and Password"}
+              onClick={signupWithEmail} 
+            />
+            <p className="p-login" >or</p>
+            <Button text={loading ? "Loading..." : "Signup Using Google"} blue={true} />
+            <p 
+              className="p-login" 
+              style={{ cursor: "pointer" }} 
+              onClick={() => setLoginForm(!loginForm)}
+            >
+              Or Have An Account Already? Click Here
+            </p>
+          </form>
+        </div>
+      )}
+      </> 
   )
 }
 
